fb46218a74a1dcc1913362b97bb602e0ef9f6cb4
Stream Protocol / StreamPay <105049869+stream-protocol@users.noreply.github.com>
2023-02-25 16:15:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["ec2-52-201-124-168.compute-1.amazonaws.com", "localhost"],
   },
 })
 
